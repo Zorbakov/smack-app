d6897e9836a824e23a6fac84c9f63c6bc5dd0ef5
Barry de Wit <barry.de.wit@alten.nl>
2018-10-17 13:58:02 +0200

    added auth service

--- smack/smack/Utilities/Constants.swift
+++ smack/smack/Utilities/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+// Type Alias
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://zorbakov-chattychat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
