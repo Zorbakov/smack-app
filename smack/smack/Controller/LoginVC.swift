79df242c839376be24030ece7bf913ef951a56c1
Barry de Wit <barry.de.wit@alten.nl>
2018-10-17 10:03:00 +0200

    added layout for login

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Barry de Wit on 17/10/2018.
+//  Copyright © 2018 Barry de Wit. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack/smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Barry de Wit on 17/10/2018.
+//  Copyright © 2018 Barry de Wit. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
